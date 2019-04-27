ae4ae2903444623a19b8323c6b8786c38dde844b
Todd Aden <FluentZap@Gmail.com>
2019-04-26 17:20:33 -0700

    add hyphen and underscore check

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -10,10 +10,12 @@ namespace WordCounter
         private List<char> _letterList = new List<char>()
         {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
+        private List<char> _nonWordEnders = new List<char>()
+        {'_', '-'};
+
         public int CountWords(string keyWord, string wordsToCheck, bool strict = false)
         {
-            int count = 0;
-            
+            int count = 0; // the number of words we have matched with
             if (!strict)
             {
                 wordsToCheck = wordsToCheck.ToLower();
@@ -21,12 +23,11 @@ namespace WordCounter
                 keyWord = _RemoveSpecialCharacters(keyWord);
             }
             
-
             char[] keyArray = keyWord.ToCharArray();
             char[] wordsArray = wordsToCheck.ToCharArray();
 
-            int k = 0;
-            int skips = 0;
+            int k = 0; //current letter in the keyword we are checking to
+            int skips = 0; // keeps track of the amount of special characters we skipped
 
             string testString = "";
             int testLetters = 0;
@@ -35,23 +36,26 @@ namespace WordCounter
             {
                 char letter = wordsArray[i];
 
-                //Check the first keyWord letter to our index in the word array
+                // check the first keyWord letter to our index in the word array
                 if (keyArray[k] == letter || (keyArray[k] == '*' && _letterList.Contains(letter)))
                 {
-                    //Found the letter now check for the seccond
+                    // found the letter now check for the seccond
                     k++;
-                    //Found word now check if it is surronded by spaces or at the start or end of the wordsArray
+                    // found word now check if it is surronded by spaces or at the start or end of the wordsArray
                     if (k == keyArray.Length)
                     {                        
                         k += skips;
                         
                         if ((i - k < 0 || // is the start of the word the start of the phrase
                             !_letterList.Contains(wordsArray[i - k]) || // is the letter before the word not a letter
-                            !_letterList.Contains(wordsArray[i - (k - skips)])) && // if it is a letter is there a symbol between them
+                            !_letterList.Contains(wordsArray[i - (k - skips)])) // if it is a letter is there a symbol between them
+                            && 
                             (i + 1 >= wordsArray.Length || // is the end of the word the end of the phrase
-                            !_letterList.Contains(wordsArray[i + 1]))) // is there a non letter that ends the word
+                                (!_letterList.Contains(wordsArray[i + 1]) // is there a non letter that ends the word
+                                && 
+                                !_nonWordEnders.Contains(wordsArray[i + 1])))) // is the non letter a hyphen or a underscore
                             count++; // if so we got one!
-                        //Reset count and start again
+                        // reset count and start again
                         k = 0;
                         skips = 0;
                     }
@@ -69,9 +73,6 @@ namespace WordCounter
             }
             return count;
         }
-
-
-
 
        private string _RemoveSpecialCharacters(string phrase)
        {
